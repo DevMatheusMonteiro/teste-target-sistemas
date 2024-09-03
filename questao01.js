ca10ebb31a3ffcd77193eee9f0f82465d05b5fb5
DevMatheusMonteiro <carlosmathmonteiro@gmail.com>
2024-09-03 19:28:18 -0300

    update questao01

--- questao01.js
+++ questao01.js
@@ -2,8 +2,7 @@ let i = 13;
 let soma = 0;
 let k = 0;
 while (k < i) {
-  k = k + 1;
-  console.log("K - " + k);
-  soma = soma + k;
-  console.log("Soma - " + soma);
+  k++;
+  soma += k;
 }
+console.log(soma);
